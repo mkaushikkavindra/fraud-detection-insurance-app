b6dd109ce7d492e297e59c7680c58833d04df098
Kaushik Kavindra M <mkaushikkavindra@gmail.com>
2025-11-25 09:23:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -382,7 +382,7 @@ elif input_mode == 'Batch File Upload':
                     with st.spinner(f"Running batch analysis on {len(df_claims)} claims..."):
                         df_results = process_claims_batch(df_claims, selected_model_function)
                         
-                        st.markdown("### ✅ Analysis Results")
+                        st.markdown("###RESULTS")
                         st.dataframe(df_results)
                         
                         # --- Download Option ---
@@ -405,6 +405,7 @@ elif input_mode == 'Analyze Proof Images':
         st.subheader("COMING SOON!")
     st.subheader("Upload the given proof images for analysis:")
     st.file_uploader("Upload an Image", type=["png", "jpg", "jpeg"])
+
 
 
 
