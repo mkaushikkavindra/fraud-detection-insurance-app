89363ccd2d411f35ef9f474d33955da1a4b89df5
Kaushik Kavindra M <mkaushikkavindra@gmail.com>
2025-11-26 12:05:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+import os
+
+st.write("Files in /pages folder:")
+st.write(os.listdir("pages"))
+
 st.set_page_config(
     page_title="Fraud Detection System",
     page_icon="🛡️",
@@ -11,6 +16,7 @@ st.write("Welcome to the Fraud Detection Web App.")
 
 if st.button("Open Fraud Score Calculator"):
     st.switch_page("score_calculator")
+
 
 
 
