09cbd2bd3679f91d1e3ae774082ab5dda2d6c9df
Kaushik Kavindra M <mkaushikkavindra@gmail.com>
2025-11-27 11:49:07 +0530

    Update How Your Fraud Risk Score is Calculated?.py

--- pages/How Your Fraud Risk Score is Calculated?.py
+++ pages/How Your Fraud Risk Score is Calculated?.py
@@ -6,4 +6,17 @@ st.set_page_config(
     initial_sidebar_state="collapsed" 
 )
 
+def logout():
+    st.session_state.logged_in = False
+    st.session_state.username = None
+    st.info("Logged out successfully. Returning to Login Page.")
+    st.switch_page("Login.py")
+
+st.sidebar.markdown(
+    f"<div style='font-weight: bold; font-size: 1.1em; ;margin-bottom: 10px;'>Welcome, {st.session_state.username}!</div>",
+    unsafe_allow_html=True
+)
+
+st.sidebar.button("Logout", on_click=logout, key="sidebar_logout_btn")
+
 st.title("How Your Fraud Risk Score is Calculated?")
