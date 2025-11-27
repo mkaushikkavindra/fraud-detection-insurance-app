b035e088693a1cb8687820a437c091b70ad9934e
Kaushik Kavindra M <mkaushikkavindra@gmail.com>
2025-11-27 08:54:23 +0530

    Update score_page.py

--- pages/score_page.py
+++ pages/score_page.py
@@ -19,13 +19,14 @@ if 'logged_in' not in st.session_state or not st.session_state.logged_in:
 
 from fraudriskscore_final import fraudriskscore_RFC, fraudriskscore_LR, fraudriskscore_GBC,fraudriskscore_final,fraudriskscore_ensemble
 
-st.set_page_config(page_title="Fraud Risk Score Calculator",initial_sidebar_state="expanded")
+st.set_page_config(page_title="Fraud Risk Score Calculator",layout="wide",initial_sidebar_state="expanded")
 
 st.sidebar.header(f"User: {st.session_state.username}")
 col_title, col_logout = st.columns([12, 1])
 with col_title:
     st.title("Car Insurance Fraud Detection")
 with col_logout:
+    st.markdown(f"**{st.session_state.username}**", unsafe_allow_html=True)
     st.markdown("<br>", unsafe_allow_html=True)
     st.button("Logout", on_click=logout, key="main_logout_btn")
 
@@ -426,6 +427,7 @@ elif input_mode == 'Analyze Proof Images':
         st.subheader("COMING SOON!")
     st.subheader("Upload the given proof images for analysis:")
     st.file_uploader("Upload an Image", type=["png", "jpg", "jpeg"])
+
 
 
 
