8d815403a8102c17da38c3bf1610a9b5d75124a3
Kaushik Kavindra M <mkaushikkavindra@gmail.com>
2025-11-30 23:22:35 +0530

    Update fraudriskscore_final.py

--- fraudriskscore_final.py
+++ fraudriskscore_final.py
@@ -203,7 +203,7 @@ def fraudriskscore_ensemble(claim: Dict[str, Any]) -> Dict[str, Any]:
         "fraud_risk_score": max_result['fraud_risk_score'],
         "text_suspicion_score": max_result['text_suspicion_score'],
         "risk_level": max_result['risk_level'],
-        "decision": max_result['decision']
+        "decision": max_result['decision'],
         #"source_model": max_result['model'],
         #"all_model_results": {resdict['model']: resdict for resdict in resultdicts}
         "model_scores": {
